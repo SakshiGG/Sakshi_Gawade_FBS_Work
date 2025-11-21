63e4197bc3e184fce22bde7172e5fdf06876ac7f
SakshiGawade <139954090+SakshiGG@users.noreply.github.com>
2025-11-21 17:59:38 +0530

    Delete Core Python/Demo/Basic directory

--- Core Python/Demo/Basic/comment.py
+++
@@ -1,9 +0,0 @@
-####Comments
-#1. for documentation
-#This is comment
-
-x = '''
-This is comment.
-This is second line comment.
-'''
-print(type(x))
--- Core Python/Demo/Basic/d1.py
+++
@@ -1,3 +0,0 @@
-print("Hello World!")
-
-# literals are any type of values
